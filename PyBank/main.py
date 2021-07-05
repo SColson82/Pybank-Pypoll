c48449b9622efba471f49afb5ff159db276d56c8
Sharon Colson <sharon.colson@gmail.com>
2021-07-05 08:52:20 -0500

    Added Greatest Monthly Decrease and the Total Months and Revenue. Pybank is finished.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,25 @@
-import os
+# Import Dependencies
 import csv
 
-#Variables:
+# Name variables:
 total_months=0
 revenue=0
 change=[]
 greatest_profit_increase=0
+greatest_profit_decrease=0
 
-# Set and open the path
+#Set and open the path to the read file
 with open("Resources/budget_data.csv",'r') as budget_data:
     budget_reader=csv.reader(budget_data)
+    #Rotate through the rows
     next(budget_reader)  
+    # Skip Headings
     row = next(budget_reader)
+    
+    # Set up initial starting points
     previous_row=int(row[1])
+    total_months += 1
+    revenue += int(row[1])
 
     for row in budget_reader:
 
@@ -28,14 +35,16 @@ with open("Resources/budget_data.csv",'r') as budget_data:
         previous_row=int(row[1])
         ave_rev_change=sum(change)/len(change)
     
-        # Find and name the greatest increase in profite (date and amount)
-        # if change in this row is greater than the change in the last row
-        # make this row the row we are keeping
+        # Greatest increase in profit (date and amount)
         if changes > greatest_profit_increase:
             greatest_profit_increase = changes
             greatest_profit_inc_mo = row[0]
+
         # Find the greatest decrease in loss (date and amount)
-      
+        if changes < greatest_profit_decrease:
+            greatest_profit_decrease = changes
+            greatest_profit_dec_mo = row[0]
+
 #  Print the analysis in Terminal
 financial_analysis = f"""
 Financial Analysis
@@ -44,7 +53,7 @@ Total Months: {total_months}
 Total: ${revenue:.2f}
 Average  Change: ${ave_rev_change:.2f}
 Greatest Increase in Profits: {greatest_profit_inc_mo} (${greatest_profit_increase:.2f})
-Greatest Decrease in Profits: Sep-2013 ($-2196167)"""
+Greatest Decrease in Profits: {greatest_profit_dec_mo} (${greatest_profit_decrease:.2f})"""
 
 print(financial_analysis)
 
