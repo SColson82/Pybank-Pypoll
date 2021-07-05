29cbe781606cc248ffc76d5a3f2c59d7b070014f
Sharon Colson <sharon.colson@gmail.com>
2021-07-05 09:30:08 -0500

    Set up Pypoll with comments, opened and read path, added dependencies and variables, and completed total vote count.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,44 @@
-# Warning: this is copy from PyBank. Don't get confused and think this is ready to go.
-
+# Import Dependencies
 import csv
-total_months = 0
-with open("Resources/budget_data.csv", "r") as budget_data:
-    budget_reader=csv.reader(budget_data)
-    column_names=next(budget_reader)
-    for row in budget_reader:
-#        print(row)
-        total_months += 1
 
-financial_analysis = f"""
-Financial Analysis
-----------------------------
-Total Months: {total_months}
-Total: $38382578
-Average  Change: $-2315.12
-Greatest Increase in Profits: Feb-2012 ($1926159)
-Greatest Decrease in Profits: Sep-2013 ($-2196167)""" 
+# Name Variables
+total_votes = 0
 
-print(financial_analysis)
+# Set and open the path to read the file
+with open("Resources/election_data.csv", "r") as election_data:
+    election_reader=csv.reader(election_data)
+    # Rotate through the rows
+    next(election_reader)
+    # Skip Headings
+    row = next(election_reader)
 
-with open("Analysis/budget_data.txt", "w") as budget_analysis:
-    budget_analysis.write(financial_analysis)
+    # Set up initial starting points
+    total_votes += 1
+    
+    for row in election_reader:
+
+        # Total number of votes cast
+        total_votes +=1
+        # Candidates who received votes
+
+        # Percentage each candidate won
+
+        # Winner by popular vote
+
+election_results = f"""
+Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+Khan: 63.000% (2218231)
+Correy: 20.000% (704200)
+Li: 14.000% (492940)
+O'Tooley: 3.000% (105630)
+-------------------------
+Winner: Khan
+-------------------------""" 
+
+print(election_results)
+
+with open("Analysis/election_data.txt", "w") as election_analysis:
+    election_analysis.write(election_results)
