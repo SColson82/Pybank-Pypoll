bed502627b4f6ab6c65a7ac19c78c1c515d48fb3
Sharon Colson <sharon.colson@gmail.com>
2021-07-05 07:57:37 -0500

    Added average monthly change, comments, and line in the 'with open' statement.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,25 +1,49 @@
+import os
 import csv
-total_months = 0
+
+#Variables:
+total_months=0
 revenue=0
+change=[]
 
-with open("Resources/budget_data.csv", "r") as budget_data:
+# Set and open the path
+with open("Resources/budget_data.csv",'r') as budget_data:
     budget_reader=csv.reader(budget_data)
-    column_names=next(budget_reader)
-    for row in budget_reader:
-#        print(row)
-        total_months += 1
-        revenue += int(row[1])
+    next(budget_reader)  
+    row = next(budget_reader)
+    previous_row=int(row[1])
 
+    for row in budget_reader:
+
+        # Total number of months in the data set
+        total_months += 1
+        
+        # Net total "Profit/Loss" for the period
+        revenue += int(row[1])
+        
+        # Average change in "Profit/Loss" over the period        
+        changes=int(row[1]) - previous_row
+        change.append(changes)
+        previous_row=int(row[1])
+        ave_rev_change=sum(change)/len(change)
+    
+        # Find and name the greatest increase in profite (date and amount)
+         
+        
+        # Find the greatest decrease in loss (date and amount)
+      
+#  Print the analysis in Terminal
 financial_analysis = f"""
 Financial Analysis
 ----------------------------
 Total Months: {total_months}
-Total: {revenue}
-Average  Change: $-2315.12
+Total: ${revenue:.2f}
+Average  Change: ${ave_rev_change:.2f}
 Greatest Increase in Profits: Feb-2012 ($1926159)
-Greatest Decrease in Profits: Sep-2013 ($-2196167)""" 
+Greatest Decrease in Profits: Sep-2013 ($-2196167)"""
 
 print(financial_analysis)
 
-with open("Analysis/budget_data.txt", "w") as budget_analysis:
+# Write to text file
+with open("Analysis/budget_data.txt","w") as budget_analysis:
     budget_analysis.write(financial_analysis)
