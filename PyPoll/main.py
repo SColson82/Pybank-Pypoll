98db325be16587a45b00d656a033c51d2b70182c
Sharon Colson <sharon.colson@gmail.com>
2021-07-06 22:09:40 -0500

    Completely rewrote Pypoll because I figured out how to use Dict. Homework finished.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,11 +3,8 @@ import csv
 
 # Name Variables
 total_votes = 0
-khan=0
-correy=0
-li=0
-otooley=0
 candidates = []
+candidate_dict={}
 
 # Set and open the path to read the file
 with open("Resources/election_data.csv", "r") as election_data:
@@ -16,58 +13,50 @@ with open("Resources/election_data.csv", "r") as election_data:
     row = next(election_reader)
  
     for row in election_reader:
-        # Candidates who received votes
+        # Gather candidates who received votes
         candidate=row[2]
         if candidate not in candidates:
             candidates.append(candidate)
-            #print(candidates) #to get and confirm list
+            candidate_dict[candidate]=0  
+            #print(candidate_dict) to get the candidate names
 
         # Total number of votes cast
         total_votes +=1
         
-        # Count votes per candidate
+        # Count votes (dictionary value) per candidate (dictionary key) and 
+        # Calculate percentage votes for each candidate 
         if row[2] == "Khan":
-            khan += 1
+            candidate_dict['Khan'] += 1
+            khan_percent = ((candidate_dict['Khan'])/total_votes)*100
         elif row[2] == "Correy":
-            correy +=1
+            candidate_dict['Correy'] += 1
+            correy_percent = ((candidate_dict['Correy'])/total_votes)*100
         elif row[2] == "Li":
-            li+=1
+            candidate_dict['Li']+= 1
+            li_percent=((candidate_dict['Li'])/total_votes)*100
         else:
-            otooley += 1
-            #print(khan) #to confirm  
-           
-        # Retrieve vote percentage
-        khan_percent=(khan/total_votes)*100
-        correy_percent=(correy/total_votes)*100
-        li_percent=(li/total_votes)*100
-        otooley_percent=(otooley/total_votes)*100
-        # print(khan_percent) #to confirm 
+            candidate_dict["O'Tooley"] += 1
+            otooley_percent=((candidate_dict["O'Tooley"])/total_votes)*100
 
-        # Winner by popular vote
-        winner = max(khan, correy, li, otooley)
-        if winner == khan:
-            winner_name = "Khan"
-        elif winner == correy:
-            winner_name = "Correy"
-        elif winner == li:
-            winner_name = "Li"
-        else:
-            winner_name = "O'Tooley"
+        # Winner by popular vote (call the Key with the Max Value in Candidate_Dict)
+        max_key=max(candidate_dict,key=candidate_dict.get)
 
+# Print results to terminal
 election_results = f"""
 Election Results
 -------------------------
 Total Votes: {total_votes}
 -------------------------
-Khan: {khan_percent:.3f}% ({khan})
-Correy: {correy_percent:.3f}% ({correy})
-Li: {li_percent:.3f}% ({li})
-O'Tooley: {otooley_percent:.3f}% ({otooley})
+Khan: {khan_percent:.3f}% ({candidate_dict['Khan']})
+Correy: {correy_percent:.3f}% ({candidate_dict['Correy']})
+Li: {li_percent:.3f}% ({candidate_dict['Li']})
+O'Tooley: {otooley_percent:.3f}% ({candidate_dict["O'Tooley"]})
 -------------------------
-Winner: {winner_name}
+Winner: {max_key}
 -------------------------""" 
 
 print(election_results)
 
+# Export results to txt file.
 with open("Analysis/election_data.txt", "w") as election_analysis:
     election_analysis.write(election_results)
